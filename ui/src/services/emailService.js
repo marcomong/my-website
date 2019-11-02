4f3b9aadca753a8d6c411a18c6108965b7f25b7d
marco <mongelli.marco.8@gmail.com>
2019-11-02 18:18:11 -0500

    removes console.log

--- ui/src/services/emailService.js
+++ ui/src/services/emailService.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 function sendEmail (payload, callback) {
-  console.log('2')
   axios.post('/email/contactMe', payload)
     .then((res) => {
       return callback(null, res)
